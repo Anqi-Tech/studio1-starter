d6fe6c9580cbb32b0be5eb3cc9d8724cb98c0e3d
Anqi-Tech <anqihu1107@gmail.com>
2026-02-06 23:29:07 -0500

    finished studio 1

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 import { createApp } from "vue";
 
 const app = createApp({
-  template: "#template",
+    template: "#template",
 
-  data() {
-    return {
-      tasks: [
-        {
-          done: false,
-          title: "",
-          active: true,
+    data() {
+        return {
+            tasks: [
+                {
+                    done: false,
+                    title: "",
+                    active: true,
+                },
+            ],
+        };
+    },
+
+    methods: {
+        addItem() {
+            this.tasks.push({});
+
+            this.setActive();
         },
-      ],
-    };
-  },
 
-  methods: {
-    addItem() {
-      this.tasks.push({});
+        deleteItem(i) {
+            this.tasks.splice(i, 1);
+            this.setActive(i - 1);
+        },
 
-      this.setActive();
+        clearCompleted() {
+            this.tasks = this.tasks.filter((task) => !task.done);
+        },
+
+        setActive(i) {
+            for (let task of this.tasks) {
+                task.active = false;
+            }
+
+            if (i >= 0 && this.tasks[i]) {
+                this.tasks[i].active = true;
+            } else if (this.tasks.length > 0) {
+                this.tasks.at(-1).active = true;
+            }
+        },
     },
-
-    deleteItem(i) {
-      this.tasks.splice(i, 1);
-      this.setActive(i - 1);
-    },
-
-    clearCompleted() {
-      this.tasks = this.tasks.filter((task) => !task.done);
-    },
-
-    setActive(i) {
-      for (let task of this.tasks) {
-        task.active = false;
-      }
-
-      if (i >= 0 && this.tasks[i]) {
-        this.tasks[i].active = true;
-      } else if (this.tasks.length > 0) {
-        this.tasks.at(-1).active = true;
-      }
-    },
-  },
 })
-  .directive("focus", { mounted: (e) => e.focus() })
-  .mount("#app");
+    .directive("focus", { mounted: (e) => e.focus() })
+    .mount("#app");
 
 // so you can run app.addItem() and app.tasks[0] in the console
 window.app = app;
